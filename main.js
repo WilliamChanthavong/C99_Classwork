785b685131b64286bbd8d295b05d6f3c2466f8b1
WilliamChanthavong <121891997+WilliamChanthavong@users.noreply.github.com>
2023-11-01 19:44:58 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -23,10 +23,10 @@ function speak()
     speak_data = document.getElementById("textbox").value;
      var utterThis = new SpeechSynthesisUtterance(speak_data);
       synth.speak(utterThis);
-      webcam.attach(camera)
+      Webcam.attach(camera)
 }
 
-webcam.set({
+Webcam.set({
     width:360,
     height:250,
     image_format : 'png',
