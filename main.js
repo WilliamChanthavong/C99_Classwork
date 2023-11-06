fc0fe66b5c7c8c2a87990da800fb3a18228173d1
WilliamChanthavong <121891997+WilliamChanthavong@users.noreply.github.com>
2023-11-05 19:34:34 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -13,17 +13,28 @@ recognition.onresult = function run (event)
     console.log(event); 
     var Content = event.results[0][0].transcript; 
     console.log(Content); 
+    if(Content=="take my selfie")
+    {
+        console.log("taking selfie ---");
+        speak();
+    }
     document.getElementById("textbox").innerHTML = Content; 
+    speak();
 }
 
 function speak()
 {
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
-     var utterThis = new SpeechSynthesisUtterance(speak_data);
-      synth.speak(utterThis);
-      Webcam.attach(camera);
-      speak();
+    // speak_data = document.getElementById("textbox").value;
+    speak_data =  "Taking your Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    }, 5000);
 }
 
 Webcam.set({
@@ -32,3 +43,19 @@ Webcam.set({
     image_format : 'png',
     png_quality:90
 });
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
